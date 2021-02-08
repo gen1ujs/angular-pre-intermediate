0a919e11b2bb6a555c8bef887e8bd32b33f6f1bc
Ömer Faruk Yeşilyurt <omerinsteami123@gmail.com>
2021-02-08 16:13:33 +0300

    ng if

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -9,45 +9,5 @@ import {ProductRepository} from './repository.model'
 export class ProductComponent{
     model : ProductRepository = new ProductRepository()
     
-    disabled:boolean = false
-    
-
-    getClasses(id:number):string{
-        const product:Product = this.model.getProductById(id)
-        return (product.price <=1000 ? "bg-primary" : "bg-secondary") + " m-2 p-2 text-white"
-    }
-
-    getClassMap(id:number):Object{
-        const product:Product=this.model.getProductById(id)
-        return {
-            "bg-info" : product.price <=1000,
-            "bg-secondary" : product.price > 1000,
-            "text-center text-white" : product.name === "Samsung S6"
-        }
-    }
-
-    color :string = "red"
-
-    getStyles(id:number):Object{
-        const product:Product=this.model.getProductById(id)
-        return {
-        fontSize:"25px",
-        color : "yellow"
-        }
-    }
-
-    onSubmit($event:any){
-        console.log('button clicked')
-        $event.stopPropagation()
-    }
-    onDiv(){
-        console.log('div clicked')
-    }
-
-
-    email:string = "omerfaruk@yesilyurt.com"
-
-    keyup(){
-        console.log(this.email)
-    }
+   
 }
